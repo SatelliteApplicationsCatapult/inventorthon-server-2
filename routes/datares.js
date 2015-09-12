2f12c7aac82c25f60f2534afc28e573c1e26e2b7
neil highley <neil@neilhighley.com>
2015-09-12 16:32:24 +0100

    add data link

--- routes/datares.js
+++ routes/datares.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/1', function(req, res, next) {
+  res.send('respond with another resource');
+});
 
 module.exports = router;
