a578672e8469fc8222505d9e62844b85768f7427
neil highley <neil@neilhighley.com>
2015-09-12 21:51:41 +0100

    add data link

--- routes/datares.js
+++ routes/datares.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs"),
+    json;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 router.get('/1', function(req, res, next) {
-  res.send('respond with another resource');
+  json = getConfig('../data1.json');
+
+
+  res.send(json);
 });
+
+
+
+
+
+function readJsonFileSync(filepath, encoding){
+
+  if (typeof (encoding) == 'undefined'){
+    encoding = 'utf8';
+  }
+  var file = fs.readFileSync(filepath, encoding);
+  return JSON.parse(file);
+}
+
+function getConfig(file){
+
+  var filepath = __dirname + '/' + file;
+  return readJsonFileSync(filepath);
+}
+
+//assume that config.json is in application root
+
 
 module.exports = router;
